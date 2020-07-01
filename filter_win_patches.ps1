bf9d2106431f3a81a72afefabc18e044576ff4aa
Petter Fors <petter.fors@siemens.com>
2020-07-01 12:53:58 +0200

    add nice exit message if fail to download file

--- filter_win_patches.ps1
+++ filter_win_patches.ps1
@@ -2,7 +2,7 @@
 # https://support.industry.siemens.com/cs/document/18490004/which-microsoft-updates-have-been-tested-for-compatibility-with-simatic-pcs-7-?dti=0&lc=en-AE
 #
 
-$excel_url = "https://support.industry.siemens.com/cs/attachments/18490004/"
+$excel_url = "https://support.industry.siemens.com/cs/attachments/18490004s/"
 
 $src_filename = "security_patches_iec.zip"
 $target_folder = $PSScriptRoot + "\tmp\"
@@ -15,8 +15,15 @@ if (-Not (Test-Path $target_folder)){
 }
 
 
+try {
+    Invoke-WebRequest -uri ($excel_url + $src_filename) -outfile ($target_folder + $src_filename)
+}
+catch {
+    $error_msg = "can not download filte from " + $excel_url + $src_filename
+    Write-Error -Message $error_msg -Category ResourceUnavailable
+    Break
+}
 
-Invoke-WebRequest -uri ($excel_url + $src_filename) -outfile ($target_folder + $src_filename)
 
 Expand-Archive  ($target_folder + $src_filename) -DestinationPath $target_folder
 $xlm_file = (Get-ChildItem $target_folder -Filter *.xml).FullName
