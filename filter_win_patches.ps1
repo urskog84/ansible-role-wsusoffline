36551193da08a6cb9d68bb9895752713d99f1627
Petter Fors <petter.fors@siemens.com>
2020-07-01 11:25:48 +0200

    Update to XML

--- filter_win_patches.ps1
+++ filter_win_patches.ps1
@@ -2,32 +2,29 @@
 # https://support.industry.siemens.com/cs/document/18490004/which-microsoft-updates-have-been-tested-for-compatibility-with-simatic-pcs-7-?dti=0&lc=en-AE
 #
 
-$excel_url = "https://support.industry.siemens.com/cs/attachments/18490004/security_patches_iec.xls"
+$excel_url = "https://support.industry.siemens.com/cs/attachments/18490004/"
 
-Remove-Item -Path .\pach_list.xls
-Remove-Item -Path .\pach_list.csv
-Remove-Item -Path .\baklist.txt
+$src_filename = "security_patches_iec.zip"
+$target_folder = $PSScriptRoot + "\tmp\"
+$out_file = $target_folder + "baklist.txt"
 
-Invoke-WebRequest -uri $excel_url -outfile .\pach_list.xls
-
-Function ExcelToCsv ($File) {
-    $myDir = $PSScriptRoot
-    $excelFile = "$myDir\" + $File
-    $csv_file = $file.split(".")[0] +  ".csv"
-    $Excel = New-Object -ComObject Excel.Application
-    $wb = $Excel.Workbooks.Open($excelFile)
-
-    foreach ($ws in $wb.Worksheets) {
-        $ws.SaveAs("$myDir\" + $csv_file, 6)
-    }
-    $Excel.Quit()
+if (-Not (Test-Path $target_folder)){
+    $folder = New-Item -Path $target_folder -ItemType Directory
+} else {
+    Get-ChildItem -Path $target_folder | Remove-Item
 }
 
-ExcelToCsv "pach_list.xls"
 
-$csv_data = Import-Csv -Path .\pach_list.csv
+
+Invoke-WebRequest -uri ($excel_url + $src_filename) -outfile ($target_folder + $src_filename)
+
+Expand-Archive  ($target_folder + $src_filename) -DestinationPath $target_folder
+$xlm_file = (Get-ChildItem $target_folder -Filter *.xml).FullName
+[xml]$data = Get-Content -path $xlm_file
+
+$csv_data = $data.PatchData.PatchVendor.Patch
 
 $kblist = $csv_data | Where-Object {$_.FailedProduct -eq 'PCS7Vxy' } | Select-Object -Property PatchIdentifier1
 
-$kblist.PatchIdentifier1 |Out-File baklist.txt
+$kblist.PatchIdentifier1 |Out-File $out_file
 
